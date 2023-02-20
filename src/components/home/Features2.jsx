ecaf6e235c01458d28586090e490d48382d7b98d
Venky <amrutwarvenkatesh@gmail.com>
2023-02-20 16:05:49 +0530

    test12

--- src/components/home/Features2.jsx
+++ src/components/home/Features2.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Features2 = () => {
     return (
         <section class="container pt-5 pb-4 mb-2 my-lg-3 my-xl-4 my-xxl-5">
-            <div class="row justify-content-center pt-2 pt-sm-3 pt-md-4 mt-lg-2 pb-lg-2 pb-xl-0">
+            {/* <div class="row justify-content-center pt-2 pt-sm-3 pt-md-4 mt-lg-2 pb-lg-2 pb-xl-0">
                 <div class="col-sm-11 col-md-9 col-lg-8 col-xl-8 col-xxl-8 text-center">
                     <h2 class="h1 mb-0">Measure & automate your community's most important KPIs across platforms</h2>
                 </div>
-            </div>
+            </div> */}
             <div class="row align-items-center py-4 py-lg-5 my-sm-2 my-md-4 my-lg-0 my-xl-2 my-xxl-4 aos-init" data-aos="fade-up" data-aos-duration="600" data-aos-offset="300" data-disable-parallax-down="md">
                 <div class="col-md-6 pt-xl-2 offset-xl-1 order-md-2 pb-3 pb-md-0 mb-3 mb-sm-4 mb-md-0">
                     <img class="d-none d-dark-mode-block" src="/assets/img/intract/feature-1.png" width="636" alt="Image" />
